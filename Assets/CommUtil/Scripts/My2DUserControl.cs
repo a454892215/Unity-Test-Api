9215994a7b2cccb5b3004085141503ae5af4da30
L <unkown@.com>
2020-01-10 16:20:44 +0800

    人物的行走跳跃等动画切换

--- Assets/CommUtil/Scripts/My2DUserControl.cs
+++ Assets/CommUtil/Scripts/My2DUserControl.cs
@@ -13,10 +13,11 @@ namespace UnityStandardAssets._2D
         const float  isGroundedCheckRadius = .2f; // 重叠圆半径 确定是否在地面
         public float x_MaxSpeed = 2f;
         public float jumpForce = 300f;
-
+        private Animator animator;
         void Start()
         {
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
             m_GroundCheckTransform = transform.Find("GroundCheck");
         }
 
@@ -46,6 +47,9 @@ namespace UnityStandardAssets._2D
                 m_Rigidbody2D.AddForce(new Vector2(0f, jumpForce)); //跳跃会和MovePosition冲突
                 print("==========AddForce===========" + transform.position.y);
             }
+
+            animator.SetBool("isGround", m_IsGrounded);
+            m_IsJump = false;
             // 是否按下左边Ctrl
             bool crouch = Input.GetKey(KeyCode.LeftControl);
 
@@ -55,7 +59,30 @@ namespace UnityStandardAssets._2D
             {
                 m_Rigidbody2D.velocity = new Vector2(h * x_MaxSpeed, m_Rigidbody2D.velocity.y);
             }
-            m_IsJump = false;
+            animator.SetFloat("xSpeed", Math.Abs(h));
+            CheckFlip(h);
+
+        }
+
+        private void CheckFlip(float h)
+        {
+            // 如果玩家不是面向右边 则向右 
+            if (h > 0 && !m_FacingRight) Flip();
+            else if (h < 0 && m_FacingRight) Flip();
+        }
+
+        private bool m_FacingRight = true;  // 是否面向右边
+
+        //转向
+        private void Flip()
+        {
+            // Switch the way the player is labelled as facing.
+            m_FacingRight = !m_FacingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
         }
     }
 }
