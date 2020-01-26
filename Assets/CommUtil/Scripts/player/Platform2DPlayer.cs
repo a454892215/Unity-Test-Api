d28a03a2d283c7a6bbced4b4a7ed7ae83b249f57
LiuPan <1573802540@qq.com>
2020-01-26 21:19:56 +0800

    代码优化

--- Assets/CommUtil/Scripts/player/Platform2DPlayer.cs
+++ Assets/CommUtil/Scripts/player/Platform2DPlayer.cs
@@ -7,10 +7,11 @@ namespace CommUtil.Scripts.player
     {
         private Transform _mGroundCheckTransform;
         public float isGroundedCheckRadius = .2f; // 重叠圆半径 确定是否在地面
-        public float x_MaxSpeed = 2f;
-        public float x_MaxLimitSpeed = 3.5f;
-        public bool m_IsGrounded; // 玩家是否在地面  
+        public float xMaxSpeed = 2f;
+        public float xMaxLimitSpeed = 3.5f;
+        public bool mIsGrounded; // 玩家是否在地面  
         private Rigidbody2D _mRigidBody2D;
+
         protected override void Awake()
         {
             base.Awake();
@@ -27,16 +28,18 @@ namespace CommUtil.Scripts.player
         //更新玩家是否在地面的状态
         private void UpdatePlayerIsGround()
         {
-            m_IsGrounded = false; //默认不在地面上
+            mIsGrounded = false; //默认不在地面上
             //获取在一个圆的半径范围内的collider
-            Collider2D[] colliderArr = Physics2D.OverlapCircleAll(_mGroundCheckTransform.position, isGroundedCheckRadius);
-            for (int i = 0; i < colliderArr.Length; i++)
+            Collider2D[] colliderArr =
+                Physics2D.OverlapCircleAll(_mGroundCheckTransform.position, isGroundedCheckRadius);
+            foreach (var t in colliderArr)
             {
                 //如果角色脚部检测到任何一个非角色碰撞 并且Y轴速度小于0.01f（避免与可以向上跳的台阶碰撞误判）
-                if (colliderArr[i].gameObject != gameObject && Math.Abs(_mRigidBody2D.velocity.y) < 0.01f)
-                    m_IsGrounded = true;
+                if (t.gameObject != gameObject && Math.Abs(_mRigidBody2D.velocity.y) < 0.01f)
+                    mIsGrounded = true;
             }
-            mAnimator.SetBool("isGround", m_IsGrounded);
+
+            mAnimator.SetBool(kIsGround, mIsGrounded);
         }
 
         // Update is called once per frame
@@ -44,20 +47,21 @@ namespace CommUtil.Scripts.player
         //水平移动 值域：[-1,1]
         public void OnHorizontalMove(float h)
         {
-            if (h != 0)
+            if (Mathf.Abs(h) > 0.01f)
             {
-                float xSpeed = Mathf.Clamp(_mRigidBody2D.velocity.x + h * x_MaxSpeed, -x_MaxLimitSpeed, x_MaxLimitSpeed);
+                float xSpeed = Mathf.Clamp(_mRigidBody2D.velocity.x + h * xMaxSpeed, -xMaxLimitSpeed, xMaxLimitSpeed);
                 _mRigidBody2D.velocity = new Vector2(xSpeed, _mRigidBody2D.velocity.y);
             }
-            mAnimator.SetFloat("xSpeed", Math.Abs(h));
+
+            mAnimator.SetFloat(kXSpeed, Math.Abs(h));
         }
 
         //当点击跳跃
         public void OnClickJump(float jumpForceFactor)
         {
-            if (m_IsGrounded)
+            if (mIsGrounded)
             {
-                float horizontalForce = _mRigidBody2D.velocity.x * 100; ;
+                float horizontalForce = _mRigidBody2D.velocity.x * 100;
                 _mRigidBody2D.AddForce(new Vector2(horizontalForce, jumpForce * jumpForceFactor)); //跳跃会和MovePosition冲突
             }
         }
@@ -70,7 +74,9 @@ namespace CommUtil.Scripts.player
             else if (h < 0 && _mFacingRight) Flip();
         }
 
-        private bool _mFacingRight = true;  // 是否面向右边
+        private bool _mFacingRight = true; // 是否面向右边
+        private static readonly int kIsGround = Animator.StringToHash("isGround");
+        private static readonly int kXSpeed = Animator.StringToHash("xSpeed");
 
         //转向
         private void Flip()
@@ -78,12 +84,10 @@ namespace CommUtil.Scripts.player
             // Switch the way the player is labelled as facing.
             _mFacingRight = !_mFacingRight;
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            var transform1 = transform;
+            Vector3 theScale = transform1.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+            transform1.localScale = theScale;
         }
-
     }
-
-
 }
